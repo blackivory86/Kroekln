e5ef49abbcfe11e593abda6d4f4ca7d7884564c1
Arne Tempelhof <arne.tempelhof@bosch-softtec.com>
2014-10-31 17:30:55 +0100

    Added some files.

---
+++ Kroekln/Kroekln/KroeklnColors.swift
@@ -0,0 +1,31 @@
+//
+//  KroeklnColors.swift
+//  Kroekln
+//
+//  Created by Arne Tempelhof on 31.10.14.
+//  Copyright (c) 2014 webventil. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+   class func kroeklnBlue () -> UIColor {
+      return UIColor(red: 26.0/225.0, green: 34.0/255.0, blue: 47.0/255.0, alpha: 1)
+   }
+
+   class func kroeklnDarkGreen() -> UIColor {
+      return UIColor(red: 0/225.0, green: 144.0/255.0, blue: 177.0/255.0, alpha: 1)
+   }
+
+   class func kroeklnLightGreen() -> UIColor {
+      return UIColor(red: 0/225.0, green: 144.0/255.0, blue: 177.0/255.0, alpha: 1)
+   }
+
+   class func kroeklnBeige() -> UIColor {
+      return UIColor(red: 255.0/255.0, green: 235.0/255.0, blue: 173.0/255.0, alpha: 1)
+   }
+
+   class func kroeklnRed() -> UIColor {
+      return UIColor(red: 215.0/255.0, green: 66.0/255.0, blue: 47.0/255.0, alpha: 1)
+   }
+}
--- Kroekln/Kroekln/ViewController.swift
+++ Kroekln/Kroekln/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var userImage: UIImageView!
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
+      userImage.layer.cornerRadius = userImage.frame.width/2
+      userImage.layer.masksToBounds = false
+      userImage.clipsToBounds = true
    }
 
    override func didReceiveMemoryWarning() {
